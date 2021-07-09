18f05b481762502b87e55e2ff5ccd162df0bb2a6
Ali <avohra@elevatesaas.com>
2021-07-09 13:48:43 +0530

    testclass implemented

---
+++ Sub.Consumer/Class1.cs
@@ -0,0 +1,16 @@
+﻿using Sub.Models;
+using System;
+
+namespace Sub.Consumer
+{
+    public class Class1
+    {
+        TestClass test = new TestClass
+        {
+            Id = 1,
+            Name = "",
+            Address = "",
+            City = ""
+        };
+    }
+}
